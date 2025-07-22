9d5e19b853abb41757bf62e04c81ed424d650043
alexmacielferreira1 <alexmacielferreira@gmail.com>
2025-07-21 21:27:01 -0300

    Adicionando a contagem e publicando

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoevento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoevento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000*60*60*24;
+    const horaEmMs = 1000*60*60;
+    const minutosEmMs = 1000*60;
+    const segundosEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento} dias, ${horasAteOEvento} horas, ${minutosAteOEvento} minutos, e ${segundosAteOEvento} segundos`;
+
+    if (distanciaAteOEvento< 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `0 dias! o evento já começou ou terminou!`
+    }
+}, 1000);
